a5eb5bca0f782414d646b981d90b37590111a994
somnathbachal023@gmail.com <somnathbachal023@gmail.com>
2020-09-07 20:59:56 +0530

    Updates

---
+++ precache-manifest.81dbb2a99b8e9a7251d289646883a0c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a32b51b9fde95018f498a9e062eef033",
+    "url": "/travel_site/index.html"
+  },
+  {
+    "revision": "0f432026e3a56aa7d498",
+    "url": "/travel_site/static/css/2.db6f3515.chunk.css"
+  },
+  {
+    "revision": "370d93482ecef57a528f",
+    "url": "/travel_site/static/css/main.4cc8e88c.chunk.css"
+  },
+  {
+    "revision": "0f432026e3a56aa7d498",
+    "url": "/travel_site/static/js/2.a08466ba.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/travel_site/static/js/2.a08466ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "370d93482ecef57a528f",
+    "url": "/travel_site/static/js/main.e57fec23.chunk.js"
+  },
+  {
+    "revision": "7b93f1a65120232d571c",
+    "url": "/travel_site/static/js/runtime-main.3417b9b5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/travel_site/static/media/logo.5d5d9eef.svg"
+  }
+]);
